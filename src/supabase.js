b42273f507ad82c2c1668692598735c28c30f159
senseibarbosagaming-rgb <senseibarbosagaming@gmail.com>
2026-05-14 13:34:20 +0100

    Initialize Supabase client in supabase.js

---
+++ src/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+
+const SUPABASE_URL = 'https://fbqjgifqlbjxwtamwzud.supabase.co'
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZicWpnaWZxbGJqeHd0YW13enVkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg3NTQwNDUsImV4cCI6MjA5NDMzMDA0NX0.UE97XA-TbDubZbVlT2lBRNKzh_zZSoHR-bUeTH6vOKw'
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
