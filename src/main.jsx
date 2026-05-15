044f33cdb99f368812c17248500a9264984db8b8
senseibarbosagaming-rgb <senseibarbosagaming@gmail.com>
2026-05-15 14:22:20 +0100

    Mount Zommy intro animation

--- src/main.jsx
+++ src/main.jsx
@@ -12,10 +12,12 @@ import PWAExperienceLayer from './PWAExperienceLayer'
 import TodayDashboardLayer from './TodayDashboardLayer'
 import TimelineScreen from './TimelineScreen'
 import ChildProfileCreator from './ChildProfileCreator'
+import ZommyIntro from './ZommyIntro'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <ZommyIntro />
     <TodayDashboardLayer />
     <TimelineScreen />
     <CompareScreen />
