9cb56d094b4d4848d32d5ab41fbdec795fca90bb
senseibarbosagaming-rgb <senseibarbosagaming@gmail.com>
2026-05-15 15:40:10 +0100

    Mount chapter launcher layer

--- src/main.jsx
+++ src/main.jsx
@@ -15,6 +15,7 @@ import ChildProfileCreator from './ChildProfileCreator'
 import ZommyIntro from './ZommyIntro'
 import ThemeLayer from './ThemeLayer'
 import ChapterScreen from './ChapterScreen'
+import ChapterLauncherLayer from './ChapterLauncherLayer'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -30,6 +31,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <FirstRunExperience />
     <NavExperienceLayer />
     <MemoryComposer />
+    <ChapterLauncherLayer />
     <NotificationControlsLayer />
     <AccessibilitySafetyLayer />
     <PWAExperienceLayer />
