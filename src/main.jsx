6edb9f0c33b822ae3fdf0c1e7d55a403a14499b6
senseibarbosagaming-rgb <senseibarbosagaming@gmail.com>
2026-05-15 10:33:31 +0100

    Mount upgraded memory composer

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import FirstRunExperience from './FirstRunExperience'
 import NavExperienceLayer from './NavExperienceLayer'
+import MemoryComposer from './MemoryComposer'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
     <FirstRunExperience />
     <NavExperienceLayer />
+    <MemoryComposer />
   </React.StrictMode>
 )
