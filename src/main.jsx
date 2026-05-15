5c9323205148440353ea09a47ffa390b09f169ba
senseibarbosagaming-rgb <senseibarbosagaming@gmail.com>
2026-05-15 10:27:16 +0100

    Mount safer labeled nav layer

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import FirstRunExperience from './FirstRunExperience'
+import NavExperienceLayer from './NavExperienceLayer'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
     <FirstRunExperience />
+    <NavExperienceLayer />
   </React.StrictMode>
 )
