61f707e0b83dcde526267317066a1c563e04bc12
senseibarbosagaming-rgb <senseibarbosagaming@gmail.com>
2026-05-16 09:09:06 +0100

    Keep main bootstrap-only

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import FirstRunExperience from './FirstRunExperience'
-import NavExperienceLayer from './NavExperienceLayer'
-import MemoryComposer from './MemoryComposer'
-import CompareScreen from './CompareScreen'
-import NotificationControlsLayer from './NotificationControlsLayer'
-import AccessibilitySafetyLayer from './AccessibilitySafetyLayer'
-import SettingsScreen from './SettingsScreen'
-import PWAExperienceLayer from './PWAExperienceLayer'
-import HomeRitualScreen from './HomeRitualScreen'
-import TimelineScreen from './TimelineScreen'
-import ChildProfileCreator from './ChildProfileCreator'
-import ZommyIntro from './ZommyIntro'
 import ThemeLayer from './ThemeLayer'
-import ChapterScreen from './ChapterScreen'
-import ChaptersLibrary from './ChaptersLibrary'
-import MemorySavedToast from './MemorySavedToast'
-import FamilySharingScreen from './FamilySharingScreen'
-import InviteAcceptanceLayer from './InviteAcceptanceLayer'
-import FamilySharingEntryLayer from './FamilySharingEntryLayer'
-import FamilyCircleHomeLayer from './FamilyCircleHomeLayer'
 import BottomSafeAreaLayer from './BottomSafeAreaLayer'
 import { AppShellProvider } from './AppShellContext'
 
@@ -29,26 +10,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <ThemeLayer />
     <BottomSafeAreaLayer />
     <AppShellProvider>
-      <InviteAcceptanceLayer />
       <App />
-      <ZommyIntro />
-      <HomeRitualScreen />
-      <FamilyCircleHomeLayer />
-      <TimelineScreen />
-      <ChaptersLibrary />
-      <ChapterScreen />
-      <CompareScreen />
-      <SettingsScreen />
-      <FamilySharingScreen />
-      <FamilySharingEntryLayer />
-      <ChildProfileCreator />
-      <FirstRunExperience />
-      <NavExperienceLayer />
-      <MemoryComposer />
-      <MemorySavedToast />
-      <NotificationControlsLayer />
-      <AccessibilitySafetyLayer />
-      <PWAExperienceLayer />
     </AppShellProvider>
   </React.StrictMode>
 )
