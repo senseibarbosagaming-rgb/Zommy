d2e020eaba6500a803e32cd787a1b0a17eb20986
senseibarbosagaming-rgb <senseibarbosagaming@gmail.com>
2026-05-17 20:14:35 +0100

    Configure Vite for Capacitor

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/',
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
 })
